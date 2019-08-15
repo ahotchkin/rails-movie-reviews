f4bed444bca0574a2d775c7f3710225baab25386
ahotchkin <ahotchk1@yahoo.com>
2019-08-15 11:37:18 -0400

    Add relationships to models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
   belongs_to :genre
+  has_many :movie_actors
+  has_many :actors, through: :movie_actors
+  has_many :reviews
+  has_many :users, through: :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :reviews
+  has_many :movies, through: :reviews
 end
