0d8337ee4a343afafae21467243947ca3edd99a3
ahotchkin <ahotchk1@yahoo.com>
2019-08-19 16:08:26 -0400

    Add admin to users and create files for model tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_152824) do
+ActiveRecord::Schema.define(version: 2019_08_19_200355) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2019_08_15_152824) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
   end
 
 end
