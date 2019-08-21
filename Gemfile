d11de07b276ca386fc8d275a8b391dc935f6ff14
ahotchkin <ahotchk1@yahoo.com>
2019-08-21 12:24:00 -0400

    Add user model tests

--- Gemfile
+++ Gemfile
@@ -35,11 +35,13 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
-gem "nokogiri", ">= 1.10.4"
+gem 'nokogiri', '>= 1.10.4'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'capybara', '>= 2.15'
+
 end
 
 group :development do
@@ -54,10 +56,10 @@ end
 
 group :test do
   # Adds support for Capybara system testing and selenium driver
-  gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
+  gem 'webdrivers', '~> 3.0'
   # Easy installation and use of chromedriver to run system tests with Chrome
-  gem 'chromedriver-helper'
+  # gem 'chromedriver-helper'
   gem 'rspec-rails'
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,77 +1,75 @@
-# require 'rails_helper'
-#
-# RSpec.describe User, :type => :model do
-#   let(:user) {
-#     User.create(
-#       :username => "Mindy",
-#       :email => "mindy@gmail.com"
-#       :password => "password",
-#     )
-#   }
+require 'rails_helper'
 
-#
-  # let(:admin) {
-  #   User.create(
-  #     :username => "Walt",
-#       :email => "walt@gmail.com"
-  #     :password => "password",
-  #     :admin => true
-  #   )
-  # }
+RSpec.describe User, :type => :model do
+  let(:user) {
+    User.create(
+      :username => "Mindy",
+      :email => "mindy@gmail.com",
+      :password => "password",
+    )
+  }
 
-#   let(:easy_a) {
-#     Movie.create(
-#       :title => "Easy A",
-#       :summary => "After a little white lie about losing her virginity gets out, a clean cut high school girl sees her life paralleling Hester Prynne's in "The Scarlet Letter," which she is currently studying in school - until she decides to use the rumor mill to advance her social and financial standing.",
-#       :year => 2010,
-#       :genre_id => #######
-#     )
-#   }
-#
-#   let(:superstar) {
-#     Movie.create(
-#       :title => "Superstar",
-#       :summary => "Orphan Mary Katherine Gallagher, an ugly duckling at St. Monica High School, has a dream: to be kissed soulfully. She decides she can realize this dream if she becomes a superstar, so her prayers, her fantasies, and her conversations with her only friend focus on achieving super-stardom. Her big chance is a school talent contest; her main competition is Evian, the school beauty, who is dancing with Sky, the object of Mary Katherine's kiss dream. Mary Katherine gets some talent-show help from her fellow special education students, her grandma, and Jesus, and inspiration from secrets in her past. Watching are Sky, Evian, and a silent classmate.",
-#       :year => 1999,
-#     )
-#   }
-#
-#   it "is valid with a username, email, password" do
-#     expect(user).to be_valid
-#   end
-#
-#   it "is not valid without a password" do
-#     expect(User.new(username: "Name")).not_to be_valid
-#   end
-#
-#   it "is valid with an admin boolean" do
-#     expect(admin).to be_valid
-#   end
-#
-#   it "defaults to admin => false" do
-#     expect(user.admin).to eq(false)
-#   end
-#
-#   it "has many rides" do
-#     first_ride = Ride.create(:user_id => user.id, :attraction_id => roller_coaster.id)
-#     second_ride = Ride.create(:user_id => user.id, :attraction_id => ferris_wheel.id)
-#     expect(user.rides.first).to eq(first_ride)
-#     expect(user.rides.last).to eq(second_ride)
-#   end
-#
-#   it "has many attractions through rides" do
-#     user.attractions << [roller_coaster, ferris_wheel]
-#     expect(user.attractions.first).to eq(roller_coaster)
-#     expect(user.attractions.last).to eq(ferris_wheel)
-#   end
-#
-#   it "has a method 'mood' that returns 'sad' when the user is more nauseous than happy" do
-#     expect(user.mood).to eq("sad")
-#   end
-#
-#   it "has a method 'mood' that returns 'happy' when the user is more happy than nauseous" do
-#     user.update(:happiness => 7)
-#     expect(user.mood).to eq("happy")
-#   end
-#
-# end
+
+  let(:admin) {
+    User.create(
+      :username => "Walt",
+      :email => "walt@gmail.com",
+      :password => "password",
+      :admin => true
+    )
+  }
+
+  let(:comedy) {
+    Genre.create(
+      :name => "Comedy"
+    )
+  }
+
+  let(:easy_a) {
+    Movie.create(
+      :title => "Easy A",
+      :summary => "After a little white lie about losing her virginity gets out, a clean cut high school girl sees her life paralleling Hester Prynne's in 'The Scarlet Letter,' which she is currently studying in school - until she decides to use the rumor mill to advance her social and financial standing.",
+      :year => 2010,
+      :genre_id => comedy.id
+    )
+  }
+
+  let(:superstar) {
+    Movie.create(
+      :title => "Superstar",
+      :summary => "Orphan Mary Katherine Gallagher, an ugly duckling at St. Monica High School, has a dream: to be kissed soulfully. She decides she can realize this dream if she becomes a superstar, so her prayers, her fantasies, and her conversations with her only friend focus on achieving super-stardom. Her big chance is a school talent contest; her main competition is Evian, the school beauty, who is dancing with Sky, the object of Mary Katherine's kiss dream. Mary Katherine gets some talent-show help from her fellow special education students, her grandma, and Jesus, and inspiration from secrets in her past. Watching are Sky, Evian, and a silent classmate.",
+      :year => 1999,
+      :genre_id => comedy.id
+    )
+  }
+
+  it "is valid with a username, email, password" do
+    expect(user).to be_valid
+  end
+
+  it "is not valid without a password" do
+    expect(User.new(username: "Name")).not_to be_valid
+  end
+
+  it "is valid with an admin boolean" do
+    expect(admin).to be_valid
+  end
+
+  it "defaults to admin => false" do
+    expect(user.admin).to eq(false)
+  end
+
+  it "has many reviews" do
+    first_review = Review.create(:user_id => user.id, :movie_id => easy_a.id, :content => "Easy A is a great movie. Emma Stone steals the show.", :rating => 4)
+    second_review = Review.create(:user_id => user.id, :movie_id => superstar.id, :content => "Superstar is hilarious at every turn.", :rating => 4)
+    expect(user.reviews.first).to eq(first_review)
+    expect(user.reviews.last).to eq(second_review)
+  end
+
+  it "has many movies through reviews" do
+    user.movies << [easy_a, superstar]
+    expect(user.movies.first).to eq(easy_a)
+    expect(user.movies.last).to eq(superstar)
+  end
+
+end
