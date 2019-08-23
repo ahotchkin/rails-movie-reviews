08f50cfd3ea1fc93a68922985a0c9a999bd33dee
ahotchkin <ahotchk1@yahoo.com>
2019-08-23 13:16:24 -0400

    Add login form and action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,19 @@
 class SessionsController < ApplicationController
 
+  def new
+  end
+
+  def create
+    user = User.find_by(:username => params[:user][:username])
+    if user && user.authenticate(:password => params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:message] = "Incorrect username or password."
+      render :new
+    end
+  end
+
   def destroy
     session.clear
     redirect_to root_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user)
     else
       flash[:message] = "Please enter all requested information."
       render :new
