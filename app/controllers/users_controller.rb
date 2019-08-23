462edb4c828894d38dd7f23e68e9c5f9ff5e7967
ahotchkin <ahotchk1@yahoo.com>
2019-08-22 22:47:20 -0400

    Add login and logout routes and actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,26 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      binding.pry
+      redirect_to user_path(@user)
     else
       flash[:message] = "Please enter all requested information."
       render :new
     end
   end
 
+
+  def show
+  end
+
+
   private
 
     def user_params
       params.require(:user).permit(:username, :email, :password, :admin)
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  root "sessions#home"
 
   get "/signup" => "users#new"
-
   # so if a user signs up incorrectly the url re-renders as /signup
   post "/signup" => "users#create"
-  
+
+  # login
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+
+  # logout
+  delete "/logout" => "sessions#destroy"
+
   resources :reviews
   resources :movie_actors
   resources :genres
