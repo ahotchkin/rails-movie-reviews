6844845ce3f9472bd6ddf1d5e7d00501d04d6e19
ahotchkin <ahotchk1@yahoo.com>
2019-08-24 14:29:05 -0400

    Update new review form to associate review with movie from main and nested route

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,7 @@ class ReviewsController < ApplicationController
   def index
     if params[:movie_id] && @movie = Movie.find_by_id(params[:movie_id])
       # nested route
-      # should display movie title at the top of the page
+      # error page if movie id doesn't exist. add logic to account for this scenario.
       @reviews = @movie.reviews.order({ created_at: :desc })
     else
       # flash[:message] = "That movie does not exist in the database" if params[:movie_id] => needs a redirect to disappear, use @error instead?
@@ -12,12 +12,21 @@ class ReviewsController < ApplicationController
   end
 
   def new
-    @review = Review.new
+    if params[:movie_id] && @movie = Movie.find_by_id(params[:movie_id])
+      @review = @movie.reviews.build
+    else
+      # @error = "That post doesn't exist" if params[:post_id]
+      @review = Review.new
+    end
   end
 
   def create
-    review = Review.new(review_params)
-    raise params.inspect
+    review = current_user.reviews.build(review_params)
+    if review.save
+      redirect_to movie_reviews_path(review.movie)
+    else
+      render :new
+    end
   end
 
   def edit
