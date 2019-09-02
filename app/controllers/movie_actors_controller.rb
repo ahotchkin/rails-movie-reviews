3808b7dfa99d579c376a0dbda08a55f826dae5c2
ahotchkin <ahotchk1@yahoo.com>
2019-09-02 11:40:33 -0400

    Add delete function to delete actors from movies from movie show page for admin only

--- app/controllers/movie_actors_controller.rb
+++ app/controllers/movie_actors_controller.rb
@@ -1,25 +1,9 @@
 class MovieActorsController < ApplicationController
 
   def destroy
-    # it's taking the id from the actor, not the movie_actor
-
     movie_actor = MovieActor.find_by_id(params[:id])
-    raise params.inspect
-
     movie_actor.delete
     redirect_to movie_path(movie_actor.movie)
-
-
   end
-
-  # def destroy
-  #   GroupShot.where(params[:shot_id], params[:group_id]).delete
-  # end
-
-  private
-
-    def movie_actor_params
-      params.require(:movie_actor).permit(:movie_actor_id)
-    end
 
 end
